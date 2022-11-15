9df85b7b53f66fdda22196965ad6451cb89eb7e3
kronBELET <kronbelet@gmail.com>
2022-11-15 15:02:09 -0500

    a

--- pagina_principal/index.php
+++ pagina_principal/index.php
@@ -4,9 +4,10 @@
   // Controlo si el usuario ya está logueado en el sistema.
   if(isset($_SESSION['email'])){
     // Le doy la bienvenida al email.
-    echo 'Bienvenido <strong>' . $_SESSION['email'] . '</strong>, <a href="cerrar-sesion.php">cerrar sesión</a>';
+    echo 'Bienvenido <strong>' . $_SESSION['email'] ;
+    echo '</strong>, <a href="cerrar.php">Cerrar sesion</a>';
   }else{
-    // Si no está logueado lo redireccion a la página de login.");
+    // Si no está logueado lo redireccion a la página de login.";
     echo "el man no esta logueado". $_SESSION['email'] ;
   }
 ?>
@@ -20,8 +21,9 @@
     <title>coco</title>
 </head>
 <body>
-    <a href="../LOGGIN/login.html">iniciar</a>
+    <a href="../LOGGIN/login.php">iniciar</a>
     <a href="../REGISTRO/registro.html">registrarse</a>
-    <a href="cerrar.php">Cerrar</a>
+    
+    <a href="../CRUD/crud.php">CRUD</a>
 </body>
 </html>
