370da7fd37bebb52d640b35914b489c4400011da
Jose Luis  Giraldo <91024863+kronBELET@users.noreply.github.com>
2023-03-04 16:34:32 -0500

    cambio de nombre de plantilla de curso
    se cambio el nombre de la carpeta de plantilla_curso a curso_individual y el archivo

--- REGISTRO/registro.php
+++ REGISTRO/registro.php
@@ -4,60 +4,39 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+    <title>registro</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
 <body>
-<div class="container mt-5">
-    <div class="row justify-content-center">
-      <div class="col-md-6">
-        <form>
-          <div class="form-group">
-            <label for="nombre">Nombre:</label>
-            <input type="text" class="form-control" id="nombre" placeholder="Ingrese su nombre">
-          </div>
-          <div class="form-group">
-            <label for="email">Correo electrónico:</label>
-            <input type="email" class="form-control" id="email" placeholder="Ingrese su correo electrónico">
-          </div>
-          <div class="form-group">
-            <label for="mensaje">Mensaje:</label>
-            <textarea class="form-control" id="mensaje" rows="3"></textarea>
-          </div>
-          <button type="submit" class="btn btn-success">Enviar</button>
-        </form>
-      </div>
-    </div>
-  </div>
-   <!--  <div class="cajita">
-        
+
+    <div class="container">
         <form action="enviar.php" method="post">
-            <h class="txt_registro">registro</h>
-            <p class="ban">nombres <input type="text" placeholder="ingresa tus nombres" name="nombre1" required></p>
-            <p class="ban">apellidos <input type="text" placeholder="ingresa tus apellidos" name="apellido1" required></p>
-            <p class="ban">usuario <input type="text" placeholder="ingresa tu usuario" name="usuario1"></p>
-            <p class="ban">correo <input type="email" placeholder="ingresa tu correo" name="email1"></p>
-            <p class="ban">contraseña <input type="password" placeholder="crea tu contraseña" name="password1"></p>
-            <input class="btn_enviar" type="submit" value="enviar">
+            <h2 class="text-center mb-4">Registro</h2>
+            <div class="form-group">
+                <label for="nombre1">Nombres</label>
+                <input type="text" class="form-control" id="nombre1" placeholder="Ingresa tus nombres" name="nombre1" required>
+            </div>
+            <div class="form-group">
+                <label for="apellido1">Apellidos</label>
+                <input type="text" class="form-control" id="apellido1" placeholder="Ingresa tus apellidos" name="apellido1" required>
+            </div>
+            <div class="form-group">
+                <label for="usuario1">Usuario</label>
+                <input type="text" class="form-control" id="usuario1" placeholder="Ingresa tu usuario" name="usuario1">
+            </div>
+            <div class="form-group">
+                <label for="email1">Correo</label>
+                <input type="email" class="form-control" id="email1" placeholder="Ingresa tu correo" name="email1">
+            </div>
+            <div class="form-group">
+                <label for="password1">Contraseña</label>
+                <input type="password" class="form-control" id="password1" placeholder="Crea tu contraseña" name="password1">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
         </form>
-    </div> -->
-</body>
-</html>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Formulario con Bootstrap</title>
-  <!-- Incluimos los archivos CSS de Bootstrap -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-</head>
-<body>
- 
-  <!-- Incluimos los archivos JS de Bootstrap -->
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    </div> 
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </body>
 </html>
---
+++ curso_individual/plantilla_curso.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Curso individual</title>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <!-- Font Awesome CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-NyvALqyWAk6jSZU6VYUjrweRw8Z1+UT+hjJrVlGvJ0K01W8Vngku6F0DR7I0vlJfMCWlXm5jbPbx5+5cBxrOIQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+  <!-- Navigation bar -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Nombre del curso</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Lecciones</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Recursos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Foro</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Iniciar sesión</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Registrarse</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <!-- Course header -->
+  <header class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Nombre del curso</h1>
+      <p class="lead">Descripción breve del curso</p>
+      <a href="#" class="btn btn-primary">Inscribirse</a>
+    </div>
+  </header>
+
+  <!-- Course content -->
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8">
+        <h2>Lecciones</h2>
+        <div class="list-group">
+          <a href="#" class="list-group-item list-group-item-action">Lección 1: Título de la lección</a>
+</html>
