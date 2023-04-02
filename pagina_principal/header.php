06efb239e1a170d8c6cad4e1e4f28550c05293b9
DestebanJV <dejimenez04@misena.edu.co>
2023-04-02 09:38:32 -0500

    Feat: delete preloader

--- pagina_principal/header.php
+++ pagina_principal/header.php
@@ -32,17 +32,7 @@ https://templatemo.com/tm-586-scholar
 
 <body>
 
-  <!-- ***** Preloader Start ***** -->
-  <div id="js-preloader" class="js-preloader">
-    <div class="preloader-inner">
-      <span class="dot"></span>
-      <div class="dots">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-    </div>
-  </div>
+
   <!-- ***** Preloader End ***** -->
    <!-- ***** Header Area Start ***** -->
   <header class="header-area header-sticky">
